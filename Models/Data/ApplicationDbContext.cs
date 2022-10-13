c39f1ef8a0218987f1a3edf71d403551f003a8b6
Deniz Gökbaş <dng.develops@gmail.com>
2022-10-14 01:51:56 +0300

    initial commit

---
+++ Models/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Budgeting_App.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Budgeting_App.Models.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options): base(options){}
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
